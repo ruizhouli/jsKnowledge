84563a97895699f8d4a6cd3573e3360456172e28
ruizhouli <1772872909@qq.com>
2019-11-17 22:19:07 +0800

    20191117End

---
+++ 2019-11-16/商品累计/product.js
@@ -0,0 +1,32 @@
+let ary = [
+    {
+        id:0,
+        num:0,
+        price:12.5,
+        sum:0
+    },
+    {
+        id:1,
+        num:0,
+        price:10.5,
+        sum:0
+    },
+    {
+        id:2,
+        num:0,
+        price:8.5,
+        sum:0
+    },
+    {
+        id:3,
+        num:0,
+        price:8,
+        sum:0
+    },
+    {
+        id:4,
+        num:0,
+        price:14.5,
+        sum:0
+    }
+]	
